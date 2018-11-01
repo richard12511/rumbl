c974622b93d0dccedecf1505c9786b3eefdb2d1b
richard12511 <richardnew12511@gmail.com>
2018-10-31 19:16:13 -0500

    add login and logout

--- web/router.ex
+++ web/router.ex
@@ -19,7 +19,7 @@ defmodule Rumbl.Router do
 
     get "/", PageController, :index
     resources "/users", UserController, only: [:index, :show, :new, :create]
-
+    resources "/sessions", SessionController, only: [:new, :create, :delete]
   end
 
 end
