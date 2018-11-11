0186e943714acc9b3bbe7d757a692ca67f43edb8
richard12511 <richardnew12511@gmail.com>
2018-11-11 15:55:11 -0600

    add socket authentication

--- web/controllers/auth.ex
+++ web/controllers/auth.ex
@@ -13,15 +13,15 @@ defmodule Rumbl.Auth do
     user_id = get_session(conn, :user_id)
 
     cond do
-      user = conn.assigns[:current_user] -> conn
-      user = user_id && repo.get(Rumbl.User, user_id) -> assign(conn, :current_user, user)
+      user = conn.assigns[:current_user] -> put_current_user(conn, user)
+      user = user_id && repo.get(Rumbl.User, user_id) -> put_current_user(conn, user)
       true -> assign(conn, :current_user, nil)
     end
   end
 
   def login(conn, user) do
     conn
-    |> assign(:current_user, user)
+    |> put_current_user(user)
     |> put_session(:user_id, user.id)
     |> configure_session(renew: true)
   end
@@ -54,6 +54,14 @@ defmodule Rumbl.Auth do
       |> redirect(to: Helpers.page_path(conn, :index))
       |> halt()
     end
+  end
+
+  defp put_current_user(conn, user) do
+    token = Phoenix.Token.sign(conn, "user socket", user.id)
+
+    conn
+    |> assign(:current_user, user)
+    |> assign(:user_token, token)
   end
 
 end
